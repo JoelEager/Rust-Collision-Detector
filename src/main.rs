5a51973d15b18739b6b8925aafe490fb5e5b371d
Joel Eager <jee7291@rit.edu>
2018-03-21 20:56:41 -0400

    Created the SAT module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+mod sat;
+
+use sat::Point;
+
 fn main() {
-    println!("Hello, world!");
+    let poly1 = vec![Point(10, 10), Point(20, 10)];
+    let poly2 = vec![Point(10, 10), Point(20, 10)];
+    println!("Result: {}", sat::has_collided(poly1, poly2, None));
 }
---
+++ src/sat.rs
@@ -0,0 +1,5 @@
+pub struct Point(pub i32, pub i32);
+
+pub fn has_collided(poly1: Vec<Point>, poly2: Vec<Point>, max_dist: Option<i32>) -> bool {
+    return true;
+}
