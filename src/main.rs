4e74bd0a5ddccb11520ff190517f2b2a905f3193
Joel Eager <jee7291@rit.edu>
2018-03-21 21:46:55 -0400

    Updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod sat;
 use sat::Point;
 
 fn main() {
-    let poly1 = vec![Point(10, 10), Point(20, 10)];
-    let poly2 = vec![Point(10, 10), Point(20, 10)];
+    let tank = vec![Point(195, 95), Point(205, 95), Point(205, 105), Point(195, 105)];
+    let shell = vec![Point(199.5, 99.5), Point(200.5, 99.5), Point(200.5, 100.5), Point(199.5, 100.5)];
     println!("Result: {}", sat::has_collided(poly1, poly2, None));
 }
