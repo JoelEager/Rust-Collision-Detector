5ffbf73e6aa9e34a0d4e70d776595e5513cc601f
Joel Eager <jee7291@rit.edu>
2018-03-22 16:14:19 -0400

    Implemented the SAT algorithm and fixed bugs

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,11 @@ use sat::Vector;
 fn main() {
     let tank = vec![Vector(195.0, 95.0), Vector(205.0, 95.0), Vector(205.0, 105.0),
                     Vector(195.0, 105.0)];
-    let shell = vec![Vector(199.5, 99.5), Vector(200.5, 99.5), Vector(200.5, 100.5),
+    let shell1 = vec![Vector(199.5, 99.5), Vector(200.5, 99.5), Vector(200.5, 100.5),
                      Vector(199.5, 100.5)];
+    let shell2 = vec![Vector(299.5, 199.5), Vector(300.5, 199.5), Vector(300.5, 200.5),
+                     Vector(299.5, 200.5)];
 
-    println!("Result: {}", sat::has_collided(tank, shell, None));
+    println!("Result 1: {}", sat::has_collided(&tank, &shell1, None));
+    println!("Result 2: {}", sat::has_collided(&tank, &shell2, None));
 }
--- src/sat.rs
+++ src/sat.rs
@@ -1,7 +1,40 @@
 pub struct Vector(pub f64, pub f64);
 
-pub fn has_collided(poly1: Vec<Vector>, poly2: Vec<Vector>, max_dist: Option<i32>) -> bool {
+pub fn has_collided(poly1: &Vec<Vector>, poly2: &Vec<Vector>, max_dist: Option<f64>) -> bool {
     // Checks for a collision between two convex 2D polygons using separating axis theorem (SAT)
+    /*
+    poly1, poly2: The two polygons described as lists of points as tuples
+        Example: [(x1, y1), (x2, y2), (x3, y3)]
+        Note: The points list must go in sequence around the polygon
+    maxDist: The maximum distance between any two points of any two polygons that can be touching
+        If this None then the optimization check that uses it will be skipped
+    */
+    let estimated_dist = (poly1[1].0 - poly2[0].0).powi(2) + (poly1[1].1 - poly2[0].1).powi(2);
+    return match max_dist {
+        Some(max_dist) if estimated_dist > max_dist.powi(2) => false,
+        _ => run_sat(poly1, poly2)
+    };
+}
+
+fn run_sat(poly1: &Vec<Vector>, poly2: &Vec<Vector>) -> bool {
+    // Implements the actual SAT algorithm
+    let mut edges = Vec::new();
+    edges.append(&mut poly_to_edges(&poly1));
+    edges.append(&mut poly_to_edges(&poly2));
+
+    let mut axes = Vec::new();
+    for edge in edges {
+        axes.push(orthogonal(&edge));
+    }
+
+    for axis in axes {
+        if !overlap(project(&poly1, &axis), project(&poly2, &axis)) {
+            // The polys don't overlap on this axis so they can't be touching
+            return false;
+        }
+    }
+
+    // The polys overlap on all axes so they must be touching
     return true;
 }
 
@@ -10,12 +43,12 @@ fn edge_vector(point1: &Vector, point2: &Vector) -> Vector {
     return Vector(point2.0 - point1.0, point2.1 - point1.1);
 }
 
-fn poly_to_edges(poly: Vec<Vector>) -> Vec<Vector> {
+fn poly_to_edges(poly: &Vec<Vector>) -> Vec<Vector> {
     // Returns a Vec of the edges of the poly as vectors
     let mut edges = Vec::new();
 
     for index in 0..poly.len() {
-        edges.push(edge_vector(&poly[index], &poly[index % poly.len()]));
+        edges.push(edge_vector(&poly[index], &poly[(index + 1) % poly.len()]));
     }
 
     return edges;
@@ -31,7 +64,7 @@ fn dot_product(vector1: &Vector, vector2: &Vector) -> f64 {
     return vector1.0 * vector2.0 + vector1.1 * vector2.1;
 }
 
-fn project(poly: Vec<Vector>, axis: &Vector) -> Vector {
+fn project(poly: &Vec<Vector>, axis: &Vector) -> Vector {
     // Returns a vector showing how much of the poly lies along the axis
     let mut min: Option<f64> = None;
     let mut max: Option<f64> = None;
@@ -50,4 +83,9 @@ fn project(poly: Vec<Vector>, axis: &Vector) -> Vector {
     }
 
     return Vector(min.unwrap(), max.unwrap());
+}
+
+fn overlap(projection1: Vector, projection2: Vector) -> bool {
+    // Returns a boolean indicating if the two projections overlap
+    return projection1.0 <= projection2.1 && projection2.0 <= projection1.1
 }
